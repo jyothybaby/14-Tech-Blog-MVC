d1e4bd8135d3fcc1e302a0bcc62db1264a21dc1e
Jyothy Baby <jyothy.v.baby@gmail.com>
2021-09-23 22:39:49 -0400

    Comitting the code for login handlebars connected to the main handlebar

---
+++ controllers/home-routes.js
@@ -0,0 +1,56 @@
+const router = require('express').Router();
+const { Comment, Post, User } = require('../models');
+
+router.get('/', async (req, res) => {
+    try {
+      const dbPostData = await Post.findAll({
+        include: [
+            {
+              model: Post,
+              attributes: ['id', 'title', 'content'],
+            },
+          ],
+          include: [
+            {
+              model: Comment,
+              attributes: ['id', 'comment_text', 'content'],
+            },
+          ],
+          include: [
+            {
+              model: User,
+              attributes: ['username'],
+            },
+          ]
+      
+      });
+  
+      const posts = dbPostData.map((post) =>
+        post.get({ plain: true })
+      );
+  
+      res.render('homepage', {
+        posts,
+        loggedIn: req.session.loggedIn,
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  });
+
+
+  // if login button is clicked, redirect to login handlebars page
+// if on route and user is logged in, redirect to home page
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+    res.render('sign-up');
+});
+module.exports = router;
